675cdd83e5132c6431f4c6c8844a456558c30aff
CrisJumbo <tonn.raus09@gmail.com>
2024-04-05 20:55:28 +0200

    add input for a custom greeting

--- src/core/infra/adapters/ApiRepository.ts
+++ src/core/infra/adapters/ApiRepository.ts
@@ -1,7 +1,12 @@
 import { GreetingContent } from "../../../ui/widget/GreetingWidget.types";
 
-export async function getGreeting(): Promise<GreetingContent>{
-    const response = await fetch('http://localhost:8080/greeting');
+export async function getGreeting(name?: string): Promise<GreetingContent>{
+    let response;
+    if(!name) {
+       response = await fetch('http://localhost:8080/greeting');
+    } else {
+         response = await fetch(`http://localhost:8080/greeting?name=${name}`);
+    }
 
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
--- src/ui/widget/GreetingWidget.tsx
+++ src/ui/widget/GreetingWidget.tsx
@@ -5,15 +5,25 @@ import { GreetingContent } from './GreetingWidget.types';
 
 const GreetingWidget = () => {
     const [content, setContent] = useState<GreetingContent>({} as GreetingContent);
+    const [name, setName] = useState<string>('');
+
+    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    }
+
+    const handleClick = () => getGreeting(name).then(data => setContent(data)).catch(error => console.error(error));
+
     useEffect( () => {
-        getGreeting().then(data => {
-            setContent(data);
-        }).catch(error => console.error(error));
+          getGreeting().then(data => {
+              setContent(data);
+          }).catch(error => console.error(error));
     }, [])
 
   return (
     <div>
         <h1>Greeting</h1>
+        <input type="text" value={name} onChange={handleNameChange} placeholder='Enter your name' />
+        <button onClick={handleClick}>Get Greeting</button>
         <p>{content.content}</p>
     </div>
   )
